607b395d2c1ba79749f6bdbc56eb3eec5338c75a
Your Name <you@example.com>
2022-02-02 18:11:26 +0100

    Can't go on

---
+++ src/components/Main.vue
@@ -0,0 +1,44 @@
+<template>
+  <div @passedFilm="getFilmCards" class="container">
+      
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'Main',
+    //data
+    data(){
+        return{
+            apiUrl: 'https://api.themoviedb.org/3/search/movie',
+            filmsArray: [],
+
+        }
+    },
+    //metodi
+    methods:{
+        getFilmCards: function(){
+            axios
+            .get(this.apiUrl, {
+            params : {
+                api_key: fbb85452ee66bae138619692ec4e952d,
+                language: en-US,
+                query: Spiderman,
+
+                }//chiusura axios.get params
+            }//chiusura oggetto interno axios.get
+            
+            )//chiusura axios.get
+            .then( (response) => {
+                this.filmsArray = response.data.results
+                console.log(response.data.results)   //SONO FERMO QUI E NON SO COSA SUCCEDE Né FACENDO COSì Né DOPO
+            })
+        }
+    }//chiusura methods
+}//chiusura export
+</script>
+
+<style>
+
+</style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,22 +1,29 @@
 <template>
+<!-- creo div container della searchbar -->
   <div class="ms_search_section">
-      <input type="text" name="" id="" class="ms_text_area" placeholder="Cerca il titolo di un film">
-      <button class="ms_search_button">Cerca</button>
+      <input v-model="filmName" type="text" name="" id="" class="ms_text_area" placeholder="Cerca il titolo di un film">
+      <button @click="$emit('passedFilm', filmName)" class="ms_search_button">Cerca</button>
   </div>
 </template>
 
 <script>
 //Dati dell'export
 export default {
-name: 'SearchBar',
-}
-//Sezione con i Data()
+    name: 'SearchBar',
 
+    //Sezione con i Data()
+    data(){
+        return{
+        filmName: '',
+        }
+    }
+}
 
 //sezione con i methods
 
 </script>
 
 <style lang="scss" scoped>
-@import url('../assets/SearchBar.scss');
+//importo stile della search bar
+@import '../assets/SearchBar.scss';
 </style>
